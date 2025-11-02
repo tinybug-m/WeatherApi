b79e57648f1e7d122302c689ccdd45076e381780
tinybug-m <arshiarnjbar@gmail.com>
2025-11-02 18:32:03 +0330

    feat: register IHttpClientFactory by calling by calling AddHttpClient in Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 // bbf753e64a697f857269e43e6a938c7f
