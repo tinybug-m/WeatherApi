5de1a4224d7b799164e0efcdb4eb470badcbe0a6
tinybug-m <arshiarnjbar@gmail.com>
2025-11-03 05:15:40 +0330

    feat: add swagger + fix bug

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddHttpClient<WeatherClient>();
 builder.Services.AddScoped<WeatherService>();
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
